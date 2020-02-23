a11dcda3c62c69142f15add7b05649e76db30336
vincent78-devweb <55990401+vincent78-devweb@users.noreply.github.com>
2020-02-23 14:35:56 +0100

    first commit

---
+++ src/main/java/com/freebds/freebdsbackend/FreebdsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.freebds.freebdsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreebdsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreebdsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freebds/freebdsbackend/FreebdsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freebds.freebdsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreebdsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
